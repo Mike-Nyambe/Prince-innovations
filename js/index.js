460e8151868d807f358eb8b856c109e69b00e54b
Mike-Nyambe <marklinenyambe@gmail.com>
2022-06-30 23:59:45 +0200

    rg

--- js/index.js
+++ js/index.js
@@ -22,3 +22,32 @@ function show() {
 function close() {
     mainMenu.style.top = '-100%';
 }
+
+// ====sticky navbar================//
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("nav");
+    header.classList.toggle("active", window.scrollY > 0);
+});
+
+// =============background===========slider//
+var imgSlider = document.getElementById("imgSlider");
+
+var images = new Array(
+    "/images/header1.jpg",
+    "/images/about-banner.jpg",
+    "/images/tech.jpg",
+    "/images/service-web.jpg"
+);
+
+var len = images.length;
+var i = 0;
+
+function slider() {
+    if (i > len - 1) {
+        i = 0;
+    }
+    imgSlider.src = images[i];
+    i++;
+    setTimeout('slider()', 3000);
+}
